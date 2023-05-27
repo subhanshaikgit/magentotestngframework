74e03e627e74af5071febe7217134a2c1eb5c914
user <user@192.168.100.11>
2023-05-27 14:55:08 +0530

    as team member i created this Gmail automation code

---
+++ src/test/java/gmailtest/logintosendfile.java
@@ -0,0 +1,62 @@
+package gmailtest;
+
+
+
+
+
+public class logintosendfile {
+    
+	public static void main(String[] args) {
+		
+	int a[]= {1,4,9,10,45,63};
+	int even=0; 
+	int odd=0;
+	
+	for(int i=0; i<a.length; i++) {
+		
+		
+		if(a[i]%2==0) {
+			
+			even++;
+		}else {
+			
+			odd++;
+		}
+		
+		System.out.println(even);
+		//System.out.println(odd);
+	}
+        
+        
+        
+        
+        
+        
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
